5340f815f05c9f2ba1d5139d52bc43379244e2c1
ankudinov aleksander <sasha@ankudinov.org.ua>
2024-04-04 19:24:14 +0300

    1

---
+++ Package.swift
@@ -0,0 +1,67 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PredoSDK",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "PredoSDK",
+            targets: ["PredoSDK"]),
+    ],
+
+    dependencies: [
+        .package(
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework.git", from: "6.9.0"),
+        .package(
+            url: "https://github.com/facebook/facebook-ios-sdk.git", branch: "main"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "PredoSDK",
+            dependencies: [
+                
+                .target(name: "Predo"),
+                
+                .target(name: "OneSignalFramework"),
+                .target(name: "OneSignalNotifications"),
+                .target(name: "OneSignalUser"),
+                .target(name: "OneSignalOutcomes"),
+                
+                
+                .product(name: "AppsFlyerLib", package: "AppsFlyerFramework"),
+                .product(name: "FacebookCore", package: "facebook-ios-sdk"),
+                .product(name: "FacebookAEM", package: "facebook-ios-sdk"),
+            ]
+        ),
+        
+        .binaryTarget(
+            name: "OneSignalFramework",
+            path: "./Sources/OneSignalFramework.xcframework"),
+        
+        .binaryTarget(
+                name: "OneSignalNotifications",
+                path: "./Sources/OneSignalNotifications.xcframework"),
+        
+        .binaryTarget(
+            name: "OneSignalUser",
+            path: "./Sources/OneSignalUser.xcframework"),
+        
+        .binaryTarget(
+            name: "OneSignalOutcomes",
+            path: "./Sources/OneSignalOutcomes.xcframework"),
+        
+        .binaryTarget(
+            name: "Predo",
+            path: "./Sources/Predo.xcframework"),
+
+    ],
+    swiftLanguageVersions: [.v5]
+)
